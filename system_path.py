063f5fd5a36f04c11495f729228926f1ee237476
Zayn <joozain309@gmail.com>
2023-09-19 12:11:07 +0300

    done

---
+++ system_path.py
@@ -0,0 +1,5 @@
+import sys
+
+sys.path.append("/kaggle/working/satellite-images-super-resolution-using-EEGAN-in-pytorch-")
+sys.path.append("/kaggle/working/satellite-images-super-resolution-using-EEGAN-in-pytorch-/src")
+sys.path.append("/kaggle/working/satellite-images-super-resolution-using-EEGAN-in-pytorch-/src/sr")
--- train.py
+++ train.py
@@ -1,13 +1,9 @@
 from sr.utils.utils import get_config
 from sr.logger import logger
 from sr.trainer.EEGAN_trainer import EEGAN_Trainer
-import sys
 
 def main(config):
         
-    sys.path.append("/kaggle/working/satellite-images-super-resolution-using-EEGAN-in-pytorch-")
-    sys.path.append("/kaggle/working/satellite-images-super-resolution-using-EEGAN-in-pytorch-/src")
-    sys.path.append("/kaggle/working/satellite-images-super-resolution-using-EEGAN-in-pytorch-/src/sr")
     trainer = EEGAN_Trainer(config,logger)
     trainer.train()
     
